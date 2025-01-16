5053009598441d1cdc0ebd4877cf75db3b4b0c7e
Amanze Bruno <bamanze.2301387@stu.cu.edu.ng>
2025-01-16 19:52:32 +0100

    added a shadcn ui component to the project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,20 @@
+import ModeToggle from "@/components/ModeToggle";
+import { Button } from "@/components/ui/button";
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <div>
+    <div className="m-4">
       <SignedOut>
         <SignInButton mode="modal">
-          <button className="bg-red-500">
-            Sign in
-          </button> 
+          <Button>Sign in</Button>
         </SignInButton>
       </SignedOut>
       <SignedIn>
         <UserButton />
       </SignedIn>
+
+      <ModeToggle />
     </div>
   );
 }
