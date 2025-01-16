476c0344264c5172cca0dcd160e215c2bb2965e5
Amanze Bruno <bamanze.2301387@stu.cu.edu.ng>
2025-01-16 19:27:20 +0100

    setting up the authentication with clerk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+
 export default function Home() {
   return (
     <div>
-      <h1>Hello Home Page</h1>
+      <SignedOut>
+        <SignInButton mode="modal">
+          <button className="bg-red-500">
+            Sign in
+          </button>
+        </SignInButton>
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
     </div>
   );
 }
