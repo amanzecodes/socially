bf9df396866d09bdb10ef9d1fdb27b50e7a45b67
Amanze Bruno <bamanze.2301387@stu.cu.edu.ng>
2025-01-19 23:49:38 +0100

    working on the sidebar for authenticates user

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,13 +2,32 @@ import { SignInButton, SignUpButton } from "@clerk/nextjs"
 import { currentUser } from "@clerk/nextjs/server"
 import { Button } from "./ui/button"
 import { Card, CardContent, CardHeader, CardTitle } from "./ui/card"
+import { getUserByClerkId } from "@/actions/user.action"
+import Link from "next/link"
+import { Avatar, AvatarImage } from "./ui/avatar"
 
 async function Sidebar() {
 const authUser = await currentUser()
 if(!authUser) return <UnAuthenticatedSidebar />
+
+const user = await getUserByClerkId(authUser.id)
+if(!user) return null
+
+console.log({user})
   return (
-    <div>
-      Sidebar
+    <div className="stick top-20">
+      <Card>
+        <CardContent className="pt-6">
+          <div className="flex flex-col items-center text-center">
+            <Link href={`/profile/${authUser.username ?? authUser.emailAddresses[0].emailAddress.split("@")[0]}`}
+          className="flex flex-col items-center justify-center">
+            <Avatar className="w-20 h-20 border-2">
+              <AvatarImage src={user.image || '/avatar.png'} />
+            </Avatar>
+          </Link>
+          </div>
+        </CardContent>
+      </Card>
     </div>
   )
 }
