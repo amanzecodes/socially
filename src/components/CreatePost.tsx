d4babc2c08d37ce81a86e3b4f22595261b6bacbd
Amanze Bruno <bamanze.2301387@stu.cu.edu.ng>
2025-01-25 01:25:38 +0100

    added shadcn ui for textarea

--- src/components/CreatePost.tsx
+++ src/components/CreatePost.tsx
@@ -1,11 +1,20 @@
-import React from 'react'
+"use client";
+
+import { useUser } from "@clerk/nextjs";
+import { useState } from "react";
+import { Card, CardContent } from "./ui/card";
 
 function CreatePost() {
-  return (
-    <div>
-      
-    </div>
-  )
+  const { user } = useUser();
+  const [content, setContent] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
+  const [isPosting, setIsPosting] = useState(false)
+  const [showImageUpload, setShowImageUpload] = useState(false)
+
+  const handleSubmit = async () => {}
+  return <Card className="mb-6">
+    <CardContent className="space-y-4"></CardContent>
+  </Card >;
 }
 
-export default CreatePost
+export default CreatePost;
