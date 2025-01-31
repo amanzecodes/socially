949be0e3fd3a4f71d259239c4a8f7dc378d9b6ad
Amanze Bruno <bamanze.2301387@stu.cu.edu.ng>
2025-01-31 01:39:18 +0100

    created the ui to handle image uploads

--- src/components/CreatePost.tsx
+++ src/components/CreatePost.tsx
@@ -3,6 +3,10 @@
 import { useUser } from "@clerk/nextjs";
 import { useState } from "react";
 import { Card, CardContent } from "./ui/card";
+import { Avatar, AvatarImage } from "@radix-ui/react-avatar";
+import { Textarea } from "./ui/textarea";
+import { Button } from "./ui/button";
+import { ImageIcon, Loader2Icon, SendIcon } from "lucide-react";
 
 function CreatePost() {
   const { user } = useUser();
@@ -13,7 +17,57 @@ function CreatePost() {
 
   const handleSubmit = async () => {}
   return <Card className="mb-6">
-    <CardContent className="space-y-4"></CardContent>
+    <CardContent className="pt-6">
+      <div className="space-y-4">
+        <div className="flex space-x-4">
+          <Avatar className="w-10 h-10">
+          <AvatarImage src={user?.imageUrl || 'avatar.png'} />
+          </Avatar>
+          <Textarea
+            className="min-h-[100px] resize-none border-none focus:ring-0 p-0 text-base"
+            placeholder="What's on your mind?"
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+            disabled={isPosting}
+          />  
+        </div>
+
+        {/* TODO HANDLE IMAGE UPLOADS */}
+        <div>
+        <div className="flex items-center justify-between border-t pt-4">
+            <div className="flex space-x-2">
+              <Button
+                type="button"
+                variant="ghost"
+                size="sm"
+                className="text-muted-foreground hover:text-primary"
+                onClick={() => setShowImageUpload(!showImageUpload)}
+                disabled={isPosting}
+              >
+                <ImageIcon className="size-4 mr-2" />
+                Photo
+              </Button>
+            </div>
+            <Button
+              className="flex items-center"
+              onClick={handleSubmit}
+              disabled={(!content.trim() && !imageUrl) || isPosting}
+            >
+              {isPosting ? (
+                <>
+                  <Loader2Icon className="size-4 mr-2 animate-spin" />
+                  Posting...
+                </>
+              ) : (
+                <>
+                  <SendIcon className="size-4 mr-2" />
+                  Post
+                </>
+              )}
+            </Button>
+          </div>
+        </div>
+        </CardContent>
   </Card >;
 }
 
