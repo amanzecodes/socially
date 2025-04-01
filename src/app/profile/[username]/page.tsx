e81f71cb1d7fd281a00fc3ab9e567836222b6726
Amanze Bruno <bamanze.2301387@stu.cu.edu.ng>
2025-04-01 20:24:47 +0100

    fixed the error in the code

--- src/app/profile/[username]/page.tsx
+++ src/app/profile/[username]/page.tsx
@@ -1,4 +1,17 @@
-const ProfilePage = ({params}: {params: {username: string}}) => {
+import { getProfileByUsername } from "@/actions/profile.action"
+
+
+export async function generateMetadata({params}: {params: {username: string}}){
+  const user = await getProfileByUsername(params.username)
+  if(!user) return {}
+
+  return {
+    title: `${user.name ?? user.username}`,
+    description: user.bio || `Check out ${user.username}'s profile`
+  }
+} 
+
+const ProfilePage = async ({params}: {params: {username: string}}) => {
   return (
     <div>ProfilePage</div>
   )
