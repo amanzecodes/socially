ea1379540d8f84ea6aaf249de72cf0e49f20c848
Amanze Bruno <bamanze.2301387@stu.cu.edu.ng>
2025-04-02 09:17:49 +0100

    finally implemented image upload with upload thing

--- src/components/CreatePost.tsx
+++ src/components/CreatePost.tsx
@@ -8,6 +8,7 @@ import { Button } from "./ui/button";
 import { ImageIcon, Loader2Icon, SendIcon } from "lucide-react";
 import { createPost } from "@/actions/post.action";
 import toast from "react-hot-toast";
+import ImageUpload from "./ImageUpload";
 
 function CreatePost() {
   const { user } = useUser();
@@ -51,7 +52,17 @@ function CreatePost() {
           />  
         </div>
 
-        {/* TODO HANDLE IMAGE UPLOADS */}
+        {(showImageUpload || imageUrl) && (
+          <div className="border rounded-lg p-4">
+            <ImageUpload
+            endpoint="postImage"
+            value={imageUrl}
+            onChange={(url) => {
+              setImageUrl(url);
+              if(!url) setShowImageUpload(false)
+            }} />
+          </div>
+        )}
         <div>
         <div className="flex items-center justify-between border-t pt-4">
             <div className="flex space-x-2">
